b6b143f383c73acd3cac03baaa8287012c753601
dkartika <dkartika@none>
2020-02-16 12:34:03 +0800

    update OS base image to ubuntu 18.04 LTS, update tigervnc, novnc,websocketify only fr ubuntu xfce

--- src/ubuntu/install/tools.sh
+++ src/ubuntu/install/tools.sh
@@ -4,8 +4,9 @@ set -e
 
 echo "Install some common tools for further installation"
 apt-get update 
-apt-get install -y vim wget net-tools locales bzip2 \
+apt-get install -y vim wget net-tools locales bzip2 nano \
     python-numpy #used for websockify/novnc
+apt-get -y upgrade
 apt-get clean -y
 
 echo "generate locales für en_US.UTF-8"
